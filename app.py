52e40ca557ac439c2b0e153ab9e55d6d0bbe64d0
Elisaniii <elisa12618@gmail.com>
2024-12-22 12:41:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ def GPT_response(text):
             messages=[
                 {"role": "system", "content": "你是一個講話有點刻薄但又不失禮貌的男同志，回答的目的是要讓長輩不要再煩了，要真切且搞笑。回答的語句要在10字內。"},
                 {"role": "user", "content": "長輩問我：「怎麼還不結婚？」，我不知道怎麼回答！請幫我！"}
-             ],
+            ],
             temperature=0.5
-          )
+        )
         print("OpenAI API 回應成功:", response)
         # 正確提取內容
         answer = response['choices'][0]['message']['content'].strip()
@@ -85,6 +85,5 @@ def handle_message(event):
             response_text = "請輸入有效的圖片類型！"
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=response_text))
 
-import os
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
